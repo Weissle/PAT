da793ca4cd225cd9434178c29228c0e1d656c4ab
weissle <a451180404@qq.com>
2019-01-31 10:07:04 +0800

    first commit

---
+++ 1021.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<map>
+#include<set>
+using namespace std;
+class solution1021 {
+	int main() {
+		typedef set<unsigned int> mySet;
+		int n;
+		cin >> n;
+		//set<unsigned int> *p = new set<unsigned int>[n];
+		mySet *p = new mySet[n];
+		for (int i = 0; i < n; ++i) {
+			int tempn;
+			cin >> tempn;
+			for (int j = 0; j < tempn; ++j) {
+				int temp;
+				cin >> temp;
+				p[i].insert(temp);
+			}
+		}
+		cin >> n;
+		for (int i = 0; i < n; ++i)
+		{
+			int a, b;
+			cin >> a >> b;
+			a--; b--;
+			unsigned int down = p[a].size() + p[b].size();
+			unsigned int up = 0;
+			mySet *small, *big;
+			if (p[a].size() > p[b].size()) {
+				small = &p[b];
+				big = &p[a];
+			}
+			else {
+				big = &p[b];
+				small = &p[a];
+			}
+			for (auto it = small->begin(); it != small->end(); ++it) {
+				auto temp = *it;
+				if (big->find(temp) != big->end()) ++up;
+			}
+			down -= up;
+			printf("%.1f%%\n", (1.0*up / down) * 100);
+		}
+		system("pause");
+	}
+};
---
+++ 1121.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<map>
+#include<set>
+#include<vector>
+#include<algorithm>
+#include<string>
+using namespace std;
+class solution1121 {
+public:
+    int main() {
+		map<string, string> s1;
+		int n, m;
+		cin >> n;
+		for (int i = 0; i < n; ++i) {
+			string a, b;
+			cin >> a >> b;
+			s1[a] = b;
+			s1[b] = a;
+		}
+		cin >> m;
+		set<string> guest;
+		vector<string> answer;
+		for (int i = 0; i < m; ++i) {
+			string temp;
+			cin >> temp;
+			string p1 = s1[temp];
+			//no CP exist
+			if (p1 == "") {
+				answer.push_back(temp);
+			}
+			else {
+				string &per = p1;
+				
+				auto findCP = guest.find(per);
+				//CP not in guest list (yet)
+				if (findCP == guest.end()) guest.insert(temp);
+				//find CP in guest list
+				else guest.erase(*findCP);
+			}
+		}
+		for (auto i = guest.begin(); i != guest.end(); ++i) answer.push_back(*i);
+		sort(answer.begin(), answer.end());
+		cout << answer.size() << endl;
+		for (int i = 0; i < answer.size(); ++i) {
+			if (i != 0) cout << " ";
+			cout << answer[i] ;
+		}
+		return 0;
+	}
+};
+int main() {
+	solution1121 p;
+	p.main();
+	system("pause");
+	return 0;
+}
