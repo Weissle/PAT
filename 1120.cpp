d4b6d9de141d1df8c1103643a19cd970e344a097
weissle <a451180404@qq.com>
2019-02-03 21:27:52 +0800

    easy and just kill time

---
+++ 1120.cpp
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<map>
+#include<set>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<stdio.h>
+#include<math.h>
+#include<stack>
+using namespace std;
+
+class solution1120 {
+	int N;
+	set<int> id;
+public:
+	int calID(int input) {
+		int answer = 0;
+		while (input != 0) {
+			answer += input % 10;
+			input /= 10;
+		}
+		return answer;
+	}
+	int main() {
+		scanf("%d", &N);
+		for (int i = 0; i < N; ++i) {
+			int temp;
+			scanf("%d", &temp);
+			id.insert(calID(temp));
+		}
+		vector<int> p;
+		p.reserve(id.size());
+		for (auto i = id.begin(); i != id.end(); ++i) {
+			p.push_back(*i);
+		}
+		sort(p.begin(), p.end());
+		printf("%d\n", p.size());
+		for (auto i = 0; i < p.size(); ++i) {
+			if (i)printf(" ");
+			printf("%d", p[i]);
+		}
+		return 0;
+	}
+
+};
+
+int main() {
+	solution1120 p;
+	p.main();
+
+	system("pause");
+	return 0;
+}
